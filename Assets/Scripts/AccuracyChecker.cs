80dc92c68f607d4a3a0339d53e869b048b8983c7
liamcristello <liamcristello@gmail.com>
2019-07-31 12:58:54 -0400

    Project Import
    After some trouble with Git and the Oculus framework, it should all be ready now. Contains test environment scene with ability to pick up and throw cubes, as well as targets that register if cubes enter their area and then update their corresponding text elements to provide a hit counter.

---
+++ Assets/Scripts/AccuracyChecker.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class AccuracyChecker : MonoBehaviour
+{
+    private int numHit;
+    private int numMiss;
+    public TextMeshProUGUI hitCounter;
+    public TextMeshProUGUI missCounter;
+    // Start is called before the first frame update
+    void Start()
+    {
+        numHit = 0;
+        numMiss = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        hitCounter.text = "Hit: " + numHit;
+        missCounter.text = "Miss: " + numMiss;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Projectile"))
+        {
+            numHit += 1;
+        }
+    }
+}
---
+++ Assets/Scripts/GroundCollision.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundCollision : MonoBehaviour
+{
+    int numMiss;
+    // Start is called before the first frame update
+    void Start()
+    {
+        numMiss = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            numMiss += 1;
+        }
+    }
+}
