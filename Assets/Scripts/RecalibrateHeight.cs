38b77109d2a6cb347387f894c4072ad0cd1c0dfc
liamcristello <liamcristello@gmail.com>
2019-08-26 13:55:29 -0400

    Groundwork for HMD Height
    Working on being able to read HMD height and calibrate level based on it. Not fully functional just yet.

--- Assets/Scripts/RecalibrateHeight.cs
+++ Assets/Scripts/RecalibrateHeight.cs
@@ -1,10 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
+/* Script to help calibrate the game to better accomodate players of different
+ * heights, based on the HMD height as well as the distance from the HMD to
+ * the handheld contrllers.
+ */ 
 public class RecalibrateHeight : MonoBehaviour
 {
-    public OVRPlayerController player;
+    // gameObject reference to OVRPlayerController prefab
+    public GameObject player;
+    // Reference to text objects to display height and arm length
+    public TextMeshProUGUI heightDisp, armLengthDisp; 
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +22,9 @@ public class RecalibrateHeight : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyUp(KeyCode.RightShift))
+        {
+
+        }
     }
 }
