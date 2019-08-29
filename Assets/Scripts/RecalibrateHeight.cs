7975ccdb543ec77410040981ad6734bdc9128c8e
liamcristello <liamcristello@gmail.com>
2019-08-29 16:20:46 -0400

    More progress on arm tracking?
    Tracking hands has proved more difficult than expected, perusing Hood code helps but isn't totally clear because of the differences between Unity Vive and Unity Oculus.

--- Assets/Scripts/RecalibrateHeight.cs
+++ Assets/Scripts/RecalibrateHeight.cs
@@ -11,8 +11,14 @@ public class RecalibrateHeight : MonoBehaviour
 {
     // gameObject reference to OVRPlayerController prefab
     public GameObject player;
+    // gameObject reference to OVRCameraRig, part of OVRPlayerController
+    public GameObject cameraRig;
+    // gameObject references to controllers
+    public GameObject leftHand, rightHand;
+    // floats to track HMD height, as well as left and right arm lengths.
+    float height, leftArmLength, rightArmLength;
     // Reference to text objects to display height and arm length
-    public TextMeshProUGUI heightDisp, armLengthDisp;
+    public TextMeshProUGUI heightDisp, leftArmLengthDisp, rightArmLengthDisp;
     // Boolean to stop accepting new data for height and arm length
     public bool calibrationComplete;
     // Start is called before the first frame update
@@ -24,10 +30,14 @@ public class RecalibrateHeight : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        heightDisp.text = "HMD Height = " + height;
+        leftArmLengthDisp.text = "Left Arm Length = " + leftArmLength;
+        rightArmLengthDisp.text = "Right Arm Length = " + rightArmLength;
         if (!calibrationComplete)
         {
-            heightDisp.text = "HMD Height = " + player.GetComponent<OVRPlayerController>().CameraHeight;
-            armLengthDisp.text = "Arm Length = " + player.GetComponent<OVRPlayerController>().CameraHeight;
+            height = player.GetComponent<OVRPlayerController>().CameraHeight;
+            leftArmLength = Mathf.Abs(Vector3.Distance(cameraRig.transform.position, leftHand.transform.position));
+            rightArmLength = Mathf.Abs(Vector3.Distance(cameraRig.transform.position, rightHand.transform.position));
         }
         if (Input.GetKeyUp(KeyCode.RightShift))
         {
